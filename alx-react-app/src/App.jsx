52233955334360193627c02161413ac25727de94
Bennah <bennah955@gmail.com>
2025-08-21 01:41:32 +0300

    Added header, main and footer(.jsx) components and intergrated them into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import WelcomeMessage from './components/WelcomeMessage'
+import HeaderContent from './components/Header'
+import MainContent from './components/MainContent'
+import FooterContent from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <HeaderContent />
+    <MainContent />
+    <FooterContent />
     <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
