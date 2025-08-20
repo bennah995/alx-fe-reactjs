564ad038c5ce5085cd81e957fe4af513ec167e9c
Bennah <bennah955@gmail.com>
2025-08-21 02:28:48 +0300

    copied the alx-react-app to new alx-react-app-new and used inline style on the components in the components folder

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function FooterContent(){
+  return(
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 My React App</p>
+    </footer>
+  )
+}
+
+export default FooterContent;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent(){
+  return(
+    <main style={{ backgroundColor: '#695151ff', padding: '20px', textAlign: 'center' }}>
+      <h2>Welcome to the React App</h2>
+      <p>This is where the main content goes.</p>
+    </main>
+  )
+}
+
+export default MainContent;
