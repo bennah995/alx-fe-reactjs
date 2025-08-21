3da12c2ea4393b4e1ea0a05d845f0c05c506aca5
Bennah <bennah955@gmail.com>
2025-08-21 16:35:06 +0300

    I created the my-company and all its work

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ background: "#333", color: "white", padding: "15px", textAlign: "center" }}>
+      <p>© {new Date().getFullYear()} My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
