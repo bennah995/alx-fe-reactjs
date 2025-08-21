534d896a55f487c1e344b2bb37419b9190e3cbd9
Bennah <bennah955@gmail.com>
2025-08-21 23:33:15 +0300

    changing case of my imports

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
 
 import RecipeList from './components/RecipeList';
-import AddRecipeform from './components/AddRecipeForm';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
      <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeform />
+      <AddRecipeForm />
       <RecipeList />
      </div>
   )
