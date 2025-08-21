9ea625053c9fad45d0292d2bb2644893026e8161
Bennah <bennah955@gmail.com>
2025-08-21 16:38:25 +0300

    added a background color to the Navdar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "15px", background: "#333", color: "white" }}>
+    <nav
+        style={{
+        backgroundColor: "#333",
+        color: "white",
+        padding: "15px",
+         isplay: "flex",
+        justifyContent: "center",
+        }}
+      >
       <Link to="/" style={{ margin: "0 15px", color: "white" }}>Home</Link>
       <Link to="/about" style={{ margin: "0 15px", color: "white" }}>About</Link>
       <Link to="/services" style={{ margin: "0 15px", color: "white" }}>Services</Link>
