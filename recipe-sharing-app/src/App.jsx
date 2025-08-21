b289281e93fad64658d19a5ae85364423040cbda
Bennah <bennah955@gmail.com>
2025-08-21 23:17:26 +0300

    created rect project and implemented zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import Recipelist from './components/RecipeList'
+import AddRecipeform from './components/AddRecipeForm'
+
+function App() {
+  return (
+     <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeform />
+      <Recipelist />
+     </div>
+  )
+}
+
+export default App
