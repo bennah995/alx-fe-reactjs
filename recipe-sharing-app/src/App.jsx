77f1e1a1324fc422fc6a038c58a63fc1820b5808
Bennah <bennah955@gmail.com>
2025-08-24 02:46:56 +0300

    delete button implementation - navigate

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -14,8 +14,7 @@ function App() {
 
       <Routes>
         <Route
-          path="/"
-          element={
+          path="/" element={
             <>
               <AddRecipeForm />
               <RecipeList />
