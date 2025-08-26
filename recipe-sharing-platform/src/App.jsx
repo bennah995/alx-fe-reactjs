fbebec8541752144c75b07adae138be90aa85076
Bennah <bennah955@gmail.com>
2025-08-27 01:01:52 +0300

    Built the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/HomePage'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-
-      <p className="text-blue-500">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='bg-gray-100 min-h-screen'>
+      < HomePage/>
+    </div>
   )
 }
 
