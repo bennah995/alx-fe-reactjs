c9070b7ad196f8f1971a679b90b12ad889e194ac
Bennah <bennah955@gmail.com>
2025-08-29 02:07:29 +0300

    Did advanced Routing in React with React Router

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export default function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      {/* Nested routes will render here */}
+      <Outlet />
+    </div>
+  );
+}
